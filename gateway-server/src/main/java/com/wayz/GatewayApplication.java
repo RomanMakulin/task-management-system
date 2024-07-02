b42aa72bdfdd295a5de3b073263ff320b36d279b
Roman Makulin <sup.makulin@mail.ru>
2024-07-02 10:53:08 +0300

    Создал и настроил окружение spring cloud, подключил eureka, настроил файлы конфигурации, Gateway, изменил миграционный файл

---
+++ gateway-server/src/main/java/com/wayz/GatewayApplication.java
@@ -0,0 +1,27 @@
+package com.wayz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("user-service",r->r.path("/users/**")
+						.uri("http://localhost:8081/"))
+				.route("order-service",r->r.path("/orders/**")
+						.uri("http://localhost:8082/")).build();}
+
+}
+
