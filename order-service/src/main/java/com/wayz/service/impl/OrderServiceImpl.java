5c490903293b37dea0eb3d9e05a9b073265d84f5
Roman Makulin <sup.makulin@mail.ru>
2024-07-03 12:51:52 +0300

    Обновлена логика отправки нотификаций через kafka

--- order-service/src/main/java/com/wayz/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/wayz/service/impl/OrderServiceImpl.java
@@ -1,12 +1,17 @@
 package com.wayz.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.wayz.dto.Notification;
+import com.wayz.dto.User;
 import com.wayz.model.Order;
 import com.wayz.model.OrderStatus;
 import com.wayz.repository.OrderRepository;
 import com.wayz.service.OrderService;
 import com.wayz.service.UserServiceClient;
 import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -19,27 +24,50 @@ import java.util.Optional;
 @Data
 public class OrderServiceImpl implements OrderService {
 
+    private final Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
+
     private final OrderRepository orderRepository;
 
     private final UserServiceClient userServiceClient;
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    private final JsonMapper mapper;
+
     private static final String TOPIC = "order-topic";
 
-    public OrderServiceImpl(OrderRepository orderRepository, UserServiceClient userServiceClient, KafkaTemplate<String, String> kafkaTemplate) {
+    public OrderServiceImpl(OrderRepository orderRepository, UserServiceClient userServiceClient, KafkaTemplate<String, String> kafkaTemplate, JsonMapper mapper) {
         this.orderRepository = orderRepository;
         this.userServiceClient = userServiceClient;
         this.kafkaTemplate = kafkaTemplate;
+        this.mapper = mapper;
     }
 
     /**
      * Отправка order-topic в сервис нотификаций
      *
-     * @param message order или любое другое сообщение для сервиса нотификаций
+     * @param order order или любое другое сообщение для сервиса нотификаций
      */
-    public void sendOrder(String message) {
-        kafkaTemplate.send(TOPIC, message);
+    public void sendOrder(Order order) {
+        Notification notification = new Notification();
+
+        try {
+            User user = userServiceClient.getUser(order.getUserId());
+
+            Optional.ofNullable(user.getEmail()).ifPresent(notification::setEmail);
+            Optional.ofNullable(user.getFirstName()).ifPresent(notification::setFirstName);
+            Optional.ofNullable(user.getLastName()).ifPresent(notification::setLastName);
+            Optional.ofNullable(user.getPhone()).ifPresent(notification::setPhone);
+            Optional.ofNullable(user.getLogin()).ifPresent(notification::setLogin);
+
+            notification.setOrder(order);
+
+            String orderJson = mapper.writeValueAsString(notification);
+            kafkaTemplate.send(TOPIC, mapper.writeValueAsString(orderJson));
+            log.info("Информация о заказе {} отправлена в notification-service", notification);
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка отправки объекта заказа в сервис нотификаций.");
+        }
     }
 
     /**
@@ -59,10 +87,7 @@ public class OrderServiceImpl implements OrderService {
                 newOrder.setItems(orderDetails.getItems());
                 newOrder.setUserId(orderDetails.getUserId());
 
-                JsonMapper mapper = new JsonMapper();
-                String orderJson = mapper.writeValueAsString(newOrder);
-                sendOrder(orderJson);
-
+                sendOrder(orderDetails);
                 orderRepository.save(newOrder);
                 return newOrder;
             } else throw new NullPointerException("User not found. Please try again to create order.");
@@ -81,8 +106,12 @@ public class OrderServiceImpl implements OrderService {
     public Order updateOrder(Order orderDetails) {
         try {
             Optional<Order> orderToUpdate = orderRepository.findById(orderDetails.getID());
-            if (orderToUpdate.isPresent()) return orderRepository.save(orderDetails);
-            else throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Order ID must not be null");
+            if (orderToUpdate.isPresent()) {
+                orderDetails.setOrderStatus(OrderStatus.UPDATED);
+                orderRepository.save(orderDetails);
+                sendOrder(orderDetails);
+                return orderDetails;
+            } else throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Order ID must not be null");
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
